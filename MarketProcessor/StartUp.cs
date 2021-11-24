cb87c79af593ed81dd3d17f8ed06291c2f1a1e58
Stas Lekontsev <stas.lekontsv@gmail.com>
2021-11-25 01:25:46 +0400

    Add qalifiers initializator

--- MarketProcessor/StartUp.cs
+++ MarketProcessor/StartUp.cs
@@ -1,11 +1,21 @@
-﻿
+﻿using MarketProcessor.MarketConditionQualifier.Implementation;
+using MarketProcessor.MarketConditionQualifier.Interfaces;
+using System.Collections.Generic;
+
 namespace MarketProcessor
 {
-    internal static class StartUp
+    internal class StartUp
     {
+        private static Dictionary<string, IConditionQualifier> _marketQualifiers;
+        internal static Dictionary<string, IConditionQualifier> MarketQualifiers => _marketQualifiers;
+
         internal static void Init()
         {
-            // TODO: place here the objects necessary for initialization
+            _marketQualifiers = new Dictionary<string, IConditionQualifier>
+             {
+                 { "Reccurent Candle Indicator", new ReccurentCandleIndicator() }
+             };
         }
+
     }
 }
