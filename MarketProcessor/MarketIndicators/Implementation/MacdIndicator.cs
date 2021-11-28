4be00c43298243287d66890e6e1145a23462a5e9
Stas Lekontsev <stas.lekontsv@gmail.com>
2021-11-29 02:09:19 +0400

    Implement Macd Signal and Macd delta

--- MarketProcessor/MarketIndicators/Implementation/MacdIndicator.cs
+++ MarketProcessor/MarketIndicators/Implementation/MacdIndicator.cs
@@ -8,20 +8,21 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("MarketProcessor.Tests")]
 namespace MarketProcessor.MarketIndicators.Implementation
 {
-    // MACD = EMA_short(Price) - EMA_long(Price), where
-    // EMA - MA Indicator
     internal class MacdIndicator : IMarketIndicator
     {
         private Mapper _mapper = new Mapper(new MapperConfiguration(cfg => cfg.CreateMap<BaseIndicatorBlock, RecurrentIndicatorBlock>()));
         private MaIndicator _maIndicatorShortPeriod;
         private MaIndicator _maIndicatorLongPeriod;
+        private MaIndicator _smoothMaIndicator;
 
         public IndicatorType Type => IndicatorType.MACD;
 
-        public MacdIndicator(MaIndicator maIndicatorShortPeriod, MaIndicator maIndicatorLongPeriod)
+        public MacdIndicator(MaIndicator maIndicatorShortPeriod, MaIndicator maIndicatorLongPeriod,
+            MaIndicator smoothMaIndicator)
         {
             _maIndicatorShortPeriod = maIndicatorShortPeriod; 
             _maIndicatorLongPeriod = maIndicatorLongPeriod;
+            _smoothMaIndicator = smoothMaIndicator;
         }
 
         public IList<BaseIndicatorBlock> Process(IList<BaseIndicatorBlock> candleSticks)
@@ -31,13 +32,22 @@ namespace MarketProcessor.MarketIndicators.Implementation
 
             var maProcessedCandleSticksShortPeriod = _maIndicatorShortPeriod.ProcessWithMaIndicatorBlock(candleSticks);
             var maProcessedCandleSticksLongPeriod = _maIndicatorLongPeriod.ProcessWithMaIndicatorBlock(candleSticks);
+            var maProcessedCandleSticksSmooth = _smoothMaIndicator.ProcessWithMaIndicatorBlock(candleSticks);
 
             for (int currentItemIndex = 0; currentItemIndex < processedCandleSticks.Count; currentItemIndex++)
             {
-                // TODO: implement the signal MACD line, then 
-                // fill delta value (MACD value - signal MACD value)
+                // MACD = EMA_short(Price) - EMA_long(Price), where EMA - MA Indicator
                 processedCandleSticks[currentItemIndex].MacdValue = maProcessedCandleSticksShortPeriod[currentItemIndex].EmaValue -
                     maProcessedCandleSticksLongPeriod[currentItemIndex].EmaValue;
+
+                // Signal_MACD = Ema_Smooth * (EMA_short(Price) - EMA_long(Price)) =
+                // = Ema_Smooth * MACD
+                processedCandleSticks[currentItemIndex].SignalMacdValue = processedCandleSticks[currentItemIndex].MacdValue *
+                    maProcessedCandleSticksSmooth[currentItemIndex].EmaValue;
+
+                // MACD Delta = MACD - Signal_MACD
+                processedCandleSticks[currentItemIndex].MacdDelta = processedCandleSticks[currentItemIndex].MacdValue -
+                    processedCandleSticks[currentItemIndex].SignalMacdValue;
             }
 
             return processedCandleSticks.ConvertAll(i => (BaseIndicatorBlock)i);
