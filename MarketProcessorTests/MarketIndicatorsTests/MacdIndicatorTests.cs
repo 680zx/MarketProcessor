909c75aeb87a5a7cea500a26b979cb5d6fd7baa5
Stas Lekontsev <stas.lekontsv@gmail.com>
2021-12-31 14:50:37 +0400

    Implement test cases to macd indicator tests

--- MarketProcessorTests/MarketIndicatorsTests/MacdIndicatorTests.cs
+++ MarketProcessorTests/MarketIndicatorsTests/MacdIndicatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using MarketProcessor.Entities;
+using MarketProcessor.MarketIndicators.Implementation;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -28,5 +29,35 @@ namespace MarketProcessor.Tests.MarketIndicatorsTests
             new MacdIndicatorBlock { CandleStickChart = new CandleStickChart { ClosePrice = 55680.00 }, EmaValue = 57241.82 },
             new MacdIndicatorBlock { CandleStickChart = new CandleStickChart { ClosePrice = 55610.00 }, EmaValue = 57116.30 }
         };
+
+        private const int MA_INDICATOR_SHORT_PERIOD = 12;
+        private const int MA_INDICATOR_LONG_PERIOD = 26;
+        private const int MA_INDICATOR_SMOOTH = 9;
+
+        private MacdIndicator _macdIndicator = new MacdIndicator(new MaIndicator(MA_INDICATOR_SHORT_PERIOD),
+                new MaIndicator(MA_INDICATOR_LONG_PERIOD), new MaIndicator(MA_INDICATOR_SMOOTH));
+
+        [Test]
+        public void Process_SimpleValues_ProcessedAndDesiredListsAreEqual()
+        {
+            // Act
+            var result = _macdIndicator.Process(_testedCandleSticks);
+
+            // Assert
+            Assert.IsTrue(AreListsEqual(result, _desiredOutputList));
+        }
+
+        private static bool AreListsEqual(IList<BaseIndicatorBlock> list1, IList<MacdIndicatorBlock> list2)
+        {
+            for (int i = 0; i < list1.Count; i++)
+            {
+                Console.WriteLine($"result: {((MacdIndicatorBlock)list1[i]).MacdValue}\texpected: {list2[i].MacdValue}");
+                if (Math.Round(((MacdIndicatorBlock)list1[i]).MacdValue, 2) != list2[i].MacdValue ||
+                    Math.Round(((MacdIndicatorBlock)list1[i]).MacdDelta, 2) != list2[i].MacdDelta)
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
