1f1289b3a57ef45dde337d8b5a8f5422c3fa02e8
Stas Lekontsev <stas.lekontsv@gmail.com>
2022-01-05 05:04:42 +0400

    Implement ramv qaulifier

--- MarketProcessor/MarketConditionQualifiers/Implementation/RamvQualifier.cs
+++ MarketProcessor/MarketConditionQualifiers/Implementation/RamvQualifier.cs
@@ -13,8 +13,11 @@ namespace MarketProcessor.MarketConditionQualifiers.Implementation
     // V - Volume anomaly
     public class RamvQualifier : IMarketConditionQualifier
     {
+        private int _flatMarketMinScoreBorder = 10;
+
         private int _minRecurrentCandleStickPeriod = 4;
         private int _maxRecurrentCandleStickPeriod = 8;
+        private double _macdMaxDeviationFromAxisRatio = 0.2; // in percents; so 0.2 means 20% 
 
         public int MinRecurrentCandleStickPeriod
         {
@@ -53,6 +56,7 @@ namespace MarketProcessor.MarketConditionQualifiers.Implementation
             // возможно, стоит для каждого индикатора назначить переменную
             // типа bool, и уже по ним судить о состоянии рынка
             var totalScore = 0;
+            int? anomalyIndex = null;
 
             foreach (var processedCandleStickCharts in processedCandleStickChartsDict)
             {
@@ -61,13 +65,28 @@ namespace MarketProcessor.MarketConditionQualifiers.Implementation
                     case IndicatorType.RecurrentCandle:
                         totalScore += GetMarketConditionByRecurrentIndicator(processedCandleStickCharts.Value);
                         break;
+
+                    case IndicatorType.PriceAnomalySearcher:
+                        totalScore += GetCurrentConditionByPriceAnomalyIndicator(processedCandleStickCharts.Value, out anomalyIndex);
+                        break;
+
+                    case IndicatorType.MACD:
+                        totalScore += GetCurrentConditionByMacdIndicator(processedCandleStickCharts.Value, anomalyIndex);
+                        break;
+
+                    case IndicatorType.LowVolumeSearcher:
+                        totalScore += GetCurrentConditionByVolumeAnomalyIndicator(processedCandleStickCharts.Value, anomalyIndex);
+                        break;
                 }
             }
 
-            return result;
+            if (totalScore > _flatMarketMinScoreBorder)
+                return MarketConditions.Flat;
+            else
+                return MarketConditions.Undefined;
         }
 
-        public int GetMarketConditionByRecurrentIndicator(IList<BaseIndicatorBlock> candleSticks)
+        private int GetMarketConditionByRecurrentIndicator(IList<BaseIndicatorBlock> candleSticks)
         {
             int resultScore = 0;
             var recurrentCandleStickBlockList = candleSticks.Cast<RecurrentIndicatorBlock>().ToList();
@@ -85,6 +104,69 @@ namespace MarketProcessor.MarketConditionQualifiers.Implementation
                     else
                         resultScore--;
                 }
+
+            return resultScore;
+        }
+
+        private int GetCurrentConditionByPriceAnomalyIndicator(IList<BaseIndicatorBlock> candleSticks, out int? anomalyIndex)
+        {
+            const int HAS_ANOMALY_SCORE = 5;
+            const int NO_ANOMALY_SCORE = -5;
+            var priceAnomalyCandleStickBlockList = candleSticks.Cast<PriceIndicatorBlock>().ToList();
+            anomalyIndex = priceAnomalyCandleStickBlockList.Where(i => i.IsAnomaly).FirstOrDefault().CandleStickChartId;
+
+            if (priceAnomalyCandleStickBlockList.Any(i => i.IsAnomaly))
+                return HAS_ANOMALY_SCORE;
+            else 
+                return NO_ANOMALY_SCORE;
+        }
+
+        private int GetCurrentConditionByMacdIndicator(IList<BaseIndicatorBlock> candleSticks, int? anomalyIndex)
+        {
+            const int NO_ANOMALY_SCORE = -5;
+
+            if (anomalyIndex == null)
+                return NO_ANOMALY_SCORE;
+
+            int resultScore = 0;
+            var macdCandleStickBlockList = candleSticks.Cast<MacdIndicatorBlock>().ToList();
+            var minMacdDeltaValue = Math.Abs(macdCandleStickBlockList.Min(i => i.MacdDelta));
+            var maxMacdDeltaValue = Math.Abs(macdCandleStickBlockList.Max(i => i.MacdDelta));
+
+            for (int i = anomalyIndex.Value; i < macdCandleStickBlockList.Count; i++)
+            {
+                if (Math.Abs(macdCandleStickBlockList[i].MacdDelta) <= minMacdDeltaValue * _macdMaxDeviationFromAxisRatio &&
+                    Math.Abs(macdCandleStickBlockList[i].MacdDelta) <= maxMacdDeltaValue * _macdMaxDeviationFromAxisRatio)
+                    resultScore++;
+                else
+                    resultScore--;
+            }
+
+            return resultScore;
+        }
+
+        private int GetCurrentConditionByVolumeAnomalyIndicator(IList<BaseIndicatorBlock> candleSticks, int? anomalyIndex)
+        {
+            const int NO_ANOMALY_SCORE = -5;
+
+            if (anomalyIndex == null)
+                return NO_ANOMALY_SCORE;
+
+            int resultScore = 0;
+            var volCandleStickBlockList = candleSticks.Cast<VolumeIndicatorBlock>().ToList();
+
+            if (volCandleStickBlockList.Any(i => i.IsLowVolume))
+            {
+                for (int i = anomalyIndex.Value; i < volCandleStickBlockList.Count; i++)
+                {
+                    if (volCandleStickBlockList[i].IsLowVolume)
+                        resultScore++;
+                    else
+                        resultScore--;
+                }
+            }
+            else
+                return NO_ANOMALY_SCORE;
 
             return resultScore;
         }
