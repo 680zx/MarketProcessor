4e0685c9db6d6217f8b80b804e8e65596c8535e8
Stas Lekontsev <stas.lekontsv@gmail.com>
2022-01-07 20:24:45 +0400

    Add register tests

---
+++ MarketProcessorTests/RegisterTests.cs
@@ -0,0 +1,26 @@
+﻿using MarketProcessor.Enums;
+using NUnit.Framework;
+
+namespace MarketProcessor.Tests
+{
+    [TestFixture]
+    internal class RegisterTests
+    {
+        [TestCase(IndicatorType.RecurrentCandle)]
+        [TestCase(IndicatorType.MA)]
+        [TestCase(IndicatorType.MACD)]
+        [TestCase(IndicatorType.LowVolumeSearcher)]
+        [TestCase(IndicatorType.PriceAnomalySearcher)]
+        public void MarketIndicators_IndicatorTypeRecurrentCandle_ReturnedAndDesiredIndicatorsAreSame(IndicatorType indicator)
+        {
+            // Arrange
+            var regIndicator = Register.MarketIndicators[indicator];
+
+            // Act
+            var regIndicatorType = regIndicator.Type; 
+
+            // Assert
+            Assert.AreEqual(indicator, regIndicatorType);
+        }
+    }
+}
